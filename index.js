4395489717fb112e5d2a0327fee950f6f0c24835
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-26 17:10:05 +0100

    24. Now declare a length variable and assign it the length of your sorted array, and a middle variable that has the value of the length divided by 2, subtracted by 1.

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const average = (nums) => {
 
 const median = (nums) => {
   const sorted = nums.slice().sort((a, b) => a - b);
+  const length = sorted.length;
+  const middle = length / 2 - 1;
 };
 
 const range = (start, end) =>
