5b1bf87a4090e2da84fd26ee985f61a898147110
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-27 10:42:55 +0100

    42. Declare a function elemValue which takes a num parameter. The function should be empty.

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ const evalFormula = (x, cells) => {
   const idToText = (id) => cells.find((cell) => cell.id === id).value;
   const rangeRegex = /([A-J])([1-9][0-9]?):([A-J])([1-9][0-9]?) /gi;
   const rangeFromString = (num1, num2) => range(parseInt(num1), parseInt(num2));
+  const elemValue = (num) => {};
 };
 
 window.onload = () => {
