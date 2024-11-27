0c91eb1817b7b8d646127b22a55ed1b2a68c41d4
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-27 09:00:07 +0100

    26. To keep track of all of your spreadsheet's functions, declare a spreadsheetFunctions object. Using the shorthand notation syntax, set sum, average, and median as properties on the spreadsheetFunctions object.

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ const median = (nums) => {
     : sorted[Math.ceil(middle)];
 };
 
+const spreadsheetFunctions = {
+  sum,
+  average,
+  median,
+};
+
 const range = (start, end) =>
   Array(end - start + 1)
     .fill(start)
