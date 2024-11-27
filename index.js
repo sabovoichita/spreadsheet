4916ba9b5e75b51e75dc501776f0a31b0d7e8a87
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-27 16:56:08 +0100

    72. You have a slight bug. arg1 and arg2 are strings, not numbers. infixToFunction['+']("1", "2") would return 12, which is not mathematically correct. Wrap each of your infixToFunction[operator] arguments in a parseFloat() call.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const infixToFunction = {
 
 const infixEval = (str, regex) =>
   str.replace(regex, (_match, arg1, operator, arg2) =>
-    infixToFunction[operator](arg1, arg2)
+    infixToFunction[operator](parseFloat(arg1), parseFloat(arg2))
   );
 
 const isEven = (num) => num % 2 === 0;
