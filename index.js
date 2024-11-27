fcc92662f7a435443ec98bb83268cce882c47ed9
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-27 16:49:22 +0100

    68. Now that you have your infix functions, you need a way to evaluate them. Declare an infixEval function which takes two parameters, str and regex. It should implicitly return the .replace() method of str, with regex and an empty callback as the arguments.

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const infixToFunction = {
   "/": (x, y) => x / y,
 };
 
+const infixEval = (str, regex) => str.replace(regex, () => {});
+
 const isEven = (num) => num % 2 === 0;
 
 const sum = (nums) => nums.reduce((acc, el) => acc + el);
