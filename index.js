093d33b4a8865040d36e075db5cb10ce3c385f04
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-27 11:35:41 +0100

    55. The callback function should have a parameter for each capture group in the regular expression. In your case, rangeRegex has four capture groups: the first letter, the first numbers, the second letter, and the second numbers. Give your callback function four more parameters to match those capture groups: char1, num1, char2, and num2. char will be short for character.

--- index.js
+++ index.js
@@ -38,7 +38,10 @@ const evalFormula = (x, cells) => {
   const elemValue = (num) => (character) => idToText(character + num);
   const addCharacters = (character1) => (character2) => (num) =>
     charRange(character1, character2).map(elemValue(num));
-  const rangeExpanded = x.replace(rangeRegex, (match) => {});
+  const rangeExpanded = x.replace(
+    rangeRegex,
+    (match, char1, num1, char2, num2) => {}
+  );
 };
 
 window.onload = () => {
