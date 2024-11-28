d548aca35b7d567cfd2d581752448602bcc1f197
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-28 11:00:50 +0100

    99. Add a has2 property which returns whether the nums array has 2 in the values, and an increment property which returns nums with every value incremented by one.

--- index.js
+++ index.js
@@ -40,6 +40,8 @@ const spreadsheetFunctions = {
   even: (nums) => nums.filter(isEven),
   firsttwo: (nums) => nums.slice(0, 2),
   lasttwo: (nums) => nums.slice(-2),
+  has2: (nums) => nums.includes(2),
+  increment: (nums) => nums.map((num) => num + 1),
 };
 
 const applyFunction = (str) => {
