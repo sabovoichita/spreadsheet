d4e87d67fe360f34c96d4f4ffeae3ec73d075441
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-28 10:16:44 +0100

    93. Now your update() function can actually evaluate formulas. Remember that you wrote the if condition to check that a function was called. Inside your if statement, set the value of the element to be the result of your evalFormula() function. Do not pass any arguments yet.

--- index.js
+++ index.js
@@ -114,6 +114,7 @@ const update = (event) => {
   const element = event.target;
   const value = element.value.replace(/\s/g, "");
   if (!value.includes(element.id) && value.charAt(0) === "=") {
+    element.value = evalFormula();
   }
 
   console.log("updating", value.includes(element.id));
