dac45f79c2ac9c72b6ad5c6863ae2573020ed2a3
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-28 10:11:28 +0100

    90. Use the ternary operator to turn your .hasOwnProperty() call into the condition. If the object has the property, return the result of calling apply with fn and args as arguments. Otherwise, return match.

--- index.js
+++ index.js
@@ -49,6 +49,8 @@ const applyFunction = (str) => {
     spreadsheetFunctions[fn.toLowerCase()](toNumberList(args));
   return str2.replace(functionCall, (match, fn, args) =>
     spreadsheetFunctions.hasOwnProperty(fn.toLowerCase())
+      ? apply(fn, args)
+      : match
   );
 };
 
