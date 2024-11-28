e76f9bb02ea9a323eae5a89764fb109e9dd8cd98
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-28 09:35:55 +0100

    82. Declare a str2 variable, and assign it the result of calling infixEval() with noHigh and infix as arguments.

--- index.js
+++ index.js
@@ -42,6 +42,7 @@ const spreadsheetFunctions = {
 const applyFunction = (str) => {
   const noHigh = highPrecedence(str);
   const infix = /([\d.]+)([+-]([\d.]+))/;
+  const str2 = infixEval(noHigh, infix);
 };
 
 const range = (start, end) =>
