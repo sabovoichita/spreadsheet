4be3849061cf91ccc989b673a747b8a8b1b91ba3
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-28 09:23:02 +0100

    75. You should use console.log() to print the result of calling the highPrecedence function with the string "5*3".

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const highPrecedence = (str) => {
   const regex = /([\d.]+)([*\/])([\d.]+)/;
   return regex.test(str);
 };
+console.log(highPrecedence("5*3"));
 
 const isEven = (num) => num % 2 === 0;
 
