f14d5eb77fde0c3b6640ba8cc7c1cedd8c2db1e0
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-28 09:56:30 +0100

    87. Your apply function is returning the spreadsheet function, but not actually applying it. Update apply to call the function. Pass in the result of calling toNumberList with args as an argument.

--- index.js
+++ index.js
@@ -45,7 +45,8 @@ const applyFunction = (str) => {
   const str2 = infixEval(noHigh, infix);
   const functionCall = /([a-z0-9]*)\(([0-9., ]*)\)(?!.*\()/i;
   const toNumberList = (args) => args.split(",").map(parseFloat);
-  const apply = (fn, args) => spreadsheetFunctions[fn.toLowerCase()];
+  const apply = (fn, args) =>
+    spreadsheetFunctions[fn.toLowerCase()](toNumberList(args));
 };
 
 const range = (start, end) =>
