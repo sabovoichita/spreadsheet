208817b984de9532faeb1f58960605b736af2343
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-28 09:49:39 +0100

    84. Declare a toNumberList function that takes an args parameter and implicitly returns the result of splitting the args by commas. Then chain a map method to your split method and pass in parseFloat as the argument to the map method.

--- index.js
+++ index.js
@@ -44,6 +44,7 @@ const applyFunction = (str) => {
   const infix = /([\d.]+)([+-]([\d.]+))/;
   const str2 = infixEval(noHigh, infix);
   const functionCall = /([a-z0-9]*)\(([0-9., ]*)\)(?!.*\()/i;
+  const toNumberList = (args) => args.split(",").map(parseFloat);
 };
 
 const range = (start, end) =>
