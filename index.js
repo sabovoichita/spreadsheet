a5896bf28c6bbc3f37dbb4266c39db9d559940dc
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-28 09:50:20 +0100

    85. Declare an apply function that takes a fn and args parameter.

--- index.js
+++ index.js
@@ -45,6 +45,7 @@ const applyFunction = (str) => {
   const str2 = infixEval(noHigh, infix);
   const functionCall = /([a-z0-9]*)\(([0-9., ]*)\)(?!.*\()/i;
   const toNumberList = (args) => args.split(",").map(parseFloat);
+  const apply = (fn, args) => {};
 };
 
 const range = (start, end) =>
